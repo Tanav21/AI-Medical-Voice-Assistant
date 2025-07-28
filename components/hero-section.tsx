6c3a753c69b35bc64dfe74ea2ebd937df3ba63b1
Tanav <tanavmahendru2003@gmail.com>
2025-07-28 16:36:04 +0530

    video fix

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -51,7 +51,7 @@ export default function HeroSection() {
                 autoPlay
                 loop
                 className="size-full object-cover opacity-50 invert dark:opacity-35 dark:invert-0 dark:lg:opacity-75"
-                src="https://ik.imagekit.io/lrigu76hy/tailark/dna-video.mp4?updatedAt=1745736251477"
+                src="/dna.mp4"
               ></video>
             </div>
           </div>
